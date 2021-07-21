d62966526cd5f23c087b2b8659bbd7bfbce10dc2
Toshiro MPIKA <49293384+ToshhM@users.noreply.github.com>
2021-07-21 22:12:49 +0200

    Mes migrations

---
+++ migrations/Version20210720222931.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210720222931 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ingredient ADD CONSTRAINT FK_6BAF7870BCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie (id)');
+        $this->addSql('CREATE INDEX IDX_6BAF7870BCF5E72D ON ingredient (categorie_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ingredient DROP FOREIGN KEY FK_6BAF7870BCF5E72D');
+        $this->addSql('DROP INDEX IDX_6BAF7870BCF5E72D ON ingredient');
+    }
+}
